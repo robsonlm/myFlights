22014a9a7fe3d22c7f7a8d70c6a9fb44a4f500d5
robsonlm <robsonlmaraia@gmail.com>
2022-04-04 15:38:21 +0100

    Changed api URLs to https

--- src/api/endpoint.js
+++ src/api/endpoint.js
@@ -1,11 +1,11 @@
 export const GET_AUTOCOMPLETE_API_URL = (city) =>
-  `http://aviation-edge.com/v2/public/autocomplete?key=${process.env.REACT_APP_AVIATIONEDGEAPI}&city=${city}`;
+  `https://aviation-edge.com/v2/public/autocomplete?key=${process.env.REACT_APP_AVIATIONEDGEAPI}&city=${city}`;
 
 export const GET_FUTUREFLIGHT_API_URL = (departure, arrival, number) =>
-  `http://api.aviationstack.com/v1/routes?access_key=${process.env.REACT_APP_AVIATIONSTACKAPI}&dep_iata=${departure}&arr_iata=${arrival}&flight_number=${number}`;
+  `https://api.aviationstack.com/v1/routes?access_key=${process.env.REACT_APP_AVIATIONSTACKAPI}&dep_iata=${departure}&arr_iata=${arrival}&flight_number=${number}`;
 
 export const GET_REALTIMEFLIGHT_API_URL = (departure, arrival, number, date) =>
-  `http://api.aviationstack.com/v1/flights?access_key=${process.env.REACT_APP_AVIATIONSTACKAPI}&dep_iata=${departure}&arr_iata=${arrival}&flight_number=${number}&flight_date=${date}`;
+  `https://api.aviationstack.com/v1/flights?access_key=${process.env.REACT_APP_AVIATIONSTACKAPI}&dep_iata=${departure}&arr_iata=${arrival}&flight_number=${number}&flight_date=${date}`;
 
 export const GET_WEATHER_API_URL = (iata) =>
-  `http://api.weatherapi.com/v1/forecast.json?key=${process.env.REACT_APP_WEATHERAPI}&q=iata:${iata}&days=3&aqi=no&alerts=no`;
+  `https://api.weatherapi.com/v1/forecast.json?key=${process.env.REACT_APP_WEATHERAPI}&q=iata:${iata}&days=3&aqi=no&alerts=no`;
